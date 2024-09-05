8e0f61849168a3c28c60a5ec3431174ecc1a4acf
Mateusz Kupiec <157227025+m-kupiec@users.noreply.github.com>
2024-09-05 16:04:50 +0200

    Ignore .js files for linting

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", "coverage"] },
+  { ignores: ["dist", "coverage", "**/*.js"] },
   {
     extends: [
       js.configs.recommended,
